6a61757f86c391ad5441643fbed2200479ea3b48
mehul <mehulbisen1@gmail.com>
2024-02-14 19:30:00 +0530

    update10

--- Ec2instance.tf
+++ Ec2instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "my_instance" {
     ami             = "ami-0766b4b472db7e3b9"
     instance_type   = "t2.micro"
     key_name        = "irelanddd"
-    security_groups = ["sg-00f87cceadfe8e076"]
+    vpc_security_group_ids = ["sg-00f87cceadfe8e076"]
     tags = {
         Name = "instance-1"
     }
