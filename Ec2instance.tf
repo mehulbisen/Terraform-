e1c104727088909407fe5a4b188425084dd2963e
mehul <mehulbisen1@gmail.com>
2024-02-14 19:25:34 +0530

    update10

--- Ec2instance.tf
+++ Ec2instance.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instance" {
-    ami = "ami-0766b4b472db7e3b9"
-    instance_type = "t2.micro"
-    key_name = "irelanddd"
-    security_groups = ["sg-030b3242a626a099c"]
+    ami             = "ami-0766b4b472db7e3b9"
+    instance_type   = "t2.micro"
+    key_name        = "irelanddd"
+    security_groups = ["sg-00f87cceadfe8e076"]
     tags = {
         Name = "instance-1"
     }
