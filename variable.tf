ee78360dbfd9718894fbe2c93761159768f0fc8d
mehul <mehulbisen1@gmail.com>
2024-02-15 00:37:15 +0530

    teraform new

---
+++ variable.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+    region = "eu-west-1"
+}
+
+resource = "aws_instance" "my-instance" {
+    ami = var.ami_id
+    key_name = var.key_name
+    instance_type = var.instance_type
+    vpc_security_group_ids = var.vpc_security_group_ids
+    tags = {
+        Name = "instance-1"
+    }
+}
+
+#variables
+
+variables "ami_id" {
+    default = "ami-0766b4b472db7e3b9"
+}
+
+variables "key_name" {
+    default = "irelanddd"
+}
+
+variables "instance_type" {
+    default = "t2.micro"
+}
+
+variables "vpc_security_group_ids" {
+    default = ["sg-00f87cceadfe8e076"]
+}
