e3721bd7c3c21296cd1bb9f264f00ef7f5c7e146
mehul <mehulbisen1@gmail.com>
2024-02-14 19:11:34 +0530

    update9

--- Ec2instance.tf
+++ Ec2instance.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-    region = "eu-west-1a"
+    region = "eu-west-1"
 }
 
 resource "aws_instance" "my_instance" {
     ami = "ami-0766b4b472db7e3b9"
     instance_type = "t2.micro"
     key_name = "irelanddd"
-    security_groups = ["sg-00f87cceadfe8e076"]
+    security_groups = ["sg-030b3242a626a099c"]
     tags = {
         Name = "instance-1"
     }
