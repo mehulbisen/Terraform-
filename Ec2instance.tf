35370af6d9767cada8846fd3f1ac81c53b053168
mehul <mehulbisen1@gmail.com>
2024-02-14 18:47:14 +0530

    update3

--- Ec2instance.tf
+++ Ec2instance.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "eu-west-1"
 }
 
-resource "aws_instance" "my-instance" {
+resource "aws_instance" "myinstance" {
     ami = "ami-0766b4b472db7e3b9"
     instance_type = "t2.micro"
     key_name = "irelanddd"
