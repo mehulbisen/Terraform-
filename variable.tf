294e388baf48f2aa18a77ca64544775a5223d2c5
mehul <mehulbisen1@gmail.com>
2024-02-15 09:00:45 +0530

    update4

--- variable.tf
+++ variable.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "eu-west-1"
 }
 
-resource = "aws_instance" "my-instance" {
+resource "aws_instance" "my-instance" {
     ami = var.ami_id
     key_name = var.key_name
     instance_type = var.instance_type
