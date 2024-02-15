365850bcb6d15369021d2d0c950a514f5eaf1ba4
mehul <mehulbisen1@gmail.com>
2024-02-15 09:55:40 +0530

    update

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "eu-west-1"
 }
 resource "aws_s3_bucket" "my_s3" {
-    bucket = "my-ireland-bucket"
+    bucket = "my-irelaaannnddd-bucket"
     tags = {
         env = "dev"
     }
