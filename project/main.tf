551b0458169d2392635600dbb7837af7b5d726d6
mehul <mehulbisen1@gmail.com>
2025-09-30 23:30:11 +0530

    test-3

--- project/main.tf
+++ project/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "vpc" {
-    source = "git::https://github.com/mehulbisen/Terraform-.git/project/modules/vpc/"
+    source = "/home/cloudshell-user/Terraform-/project/modules/vpc/"
     vpc_cidr = var.vpc_cidr 
     aws_public_subnet = var.public_subnet_cidr
     aws_private_subnet = var.private_subnet_cidr 
