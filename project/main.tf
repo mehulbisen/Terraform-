1e1ce33ecb011249dbe1d41105889e6d87ff8527
mehul <mehulbisen1@gmail.com>
2025-09-30 23:43:37 +0530

    test-4

--- project/main.tf
+++ project/main.tf
@@ -5,6 +5,6 @@ provider "aws" {
 module "vpc" {
     source = "/home/cloudshell-user/Terraform-/project/modules/vpc/"
     vpc_cidr = var.vpc_cidr 
-    aws_public_subnet = var.public_subnet_cidr
-    aws_private_subnet = var.private_subnet_cidr 
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr 
 }
--- project/variables.tf
+++ project/variables.tf
@@ -10,13 +10,13 @@ variable "vpc_cidr" {
     default = "10.0.0.0/16"
 }
 
-variable "aws_public_subnet" {
+variable "public_subnet_cidr" {
     description = "cidr for public subnet"
     type = string 
     default = "10.0.1.0/24"
 }
 
-variable "aws_private_subnet" {
+variable "private_subnet_cidr" {
     description = "cidr for private subnet" 
     type = string 
     default = "10.0.2.0/24"
